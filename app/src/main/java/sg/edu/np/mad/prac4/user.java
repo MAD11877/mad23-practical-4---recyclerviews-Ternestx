a3483b20a308cf4bb7bc4a7c579585f0833696e4
Wee Kiat Ernest Toh <s10221816@connect.np.edu.sg>
2023-06-17 00:06:11 +0800

    prac4 pls

---
+++ app/src/main/java/sg/edu/np/mad/prac4/user.java
@@ -0,0 +1,10 @@
+package sg.edu.np.mad.prac4;
+
+import java.io.Serializable;
+
+public class user implements Serializable {
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+}
